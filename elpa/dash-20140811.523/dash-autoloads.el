90f985fe002bc51d529449c2b4211e954bd9dbc4
killsto <kiliansto@gmail.com>
2014-09-29 18:59:04 -0500

    Fixed some submodules, grab some things via elpa

---
+++ elpa/dash-20140811.523/dash-autoloads.el
@@ -0,0 +1,18 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21545 60733
+;;;;;;  543193 257000))
+
+;;;***
+
+(provide 'dash-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dash-autoloads.el ends here
---
+++ elpa/epl-20140823.609/epl-autoloads.el
@@ -0,0 +1,18 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21545 60732 442809
+;;;;;;  536000))
+
+;;;***
+
+(provide 'epl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epl-autoloads.el ends here
