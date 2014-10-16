98c0cd4ea917dc7b95c71506a337bf300884cdb3
killsto <kiliansto@gmail.com>
2014-10-16 01:25:39 -0500

    changed theme, added bas16 themes

--- conf/theme.el
+++ conf/theme.el
@@ -1,9 +1,7 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/monokai-emacs")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/tango")
-(load-theme 'monokai t)
-(when (member "Ubuntu Mono" (font-family-list))
-  (set-face-attribute 'default nil :font "Ubuntu Mono"))
+(load-theme 'tango-dark t)
 (when (member "Anonymous Pro" (font-family-list))
       (set-face-attribute 'default nil :font "Anonymous Pro"))
 
