e271445e82d8abd8b38a9dcf375708d80c884860
Killian Stone <kiliansto@gmail.com>
2015-01-15 20:26:20 -0600

    fixed theme and cursor

--- conf/default-modes.el
+++ conf/default-modes.el
@@ -61,3 +61,5 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(setq default-cursor-type '(bar . 1))
